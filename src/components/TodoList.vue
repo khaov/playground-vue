795a1bfd2dfe42d542c554d0ddf15bce2dbd2842
Artyom Khaov <a.khaov@gmail.com>
2024-08-29 17:03:50 +0700

    feat: Creating addTask function, task input using v-model, add task button using @click

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,14 +6,37 @@ const todos = ref([
   { id: '2', title: 'Задача 2', completed: false },
   { id: '3', title: 'Задача 3', completed: true }
 ])
+
+const inputTask = ref('')
+
+function addTask() {
+  const taskID = (todos.value.length + 1).toString()
+  if (inputTask.value.length) {
+    todos.value.push({
+      id: taskID,
+      title: inputTask.value,
+      completed: false
+    })
+  }
+
+  inputTask.value = ''
+}
 </script>
 
 <template>
-  <h1>Список дел</h1>
+  <h1>Список задач</h1>
+
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       <input :id="todo.id" type="checkbox" v-model="todo.completed" />
       <label :for="todo.id">{{ todo.title }}</label>
     </li>
   </ul>
+
+  <h2>Добавить задачу</h2>
+
+  <form>
+    <input v-model="inputTask" type="text" placeholder="Сформулируйте задачу" />
+    <button @click="addTask" type="button">Добавить</button>
+  </form>
 </template>
