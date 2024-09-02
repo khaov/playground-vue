8c70cbc15269bb9da3c52f5b8f1f9d0e7d9f73cf
Artyom Khaov <a.khaov@gmail.com>
2024-09-02 15:54:41 +0700

    feat: Creating filteredTodos computed property for filtering tasks

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -15,10 +15,23 @@ const unfinishedTasks = computed(() => {
   return todos.value.length - completedTasks.value
 })
 
+const filter = ref('all')
+
+const filteredTodos = computed(() => {
+  if (filter.value === 'all') {
+    return todos.value
+  } else if (filter.value === 'completed') {
+    return todos.value.filter((todo) => todo.completed)
+  } else {
+    return todos.value.filter((todo) => !todo.completed)
+  }
+})
+
 const inputTask = ref('')
 
 function addTask() {
   const taskID = (todos.value.length + 1).toString()
+
   if (inputTask.value.length) {
     todos.value.push({
       id: taskID,
@@ -33,11 +46,23 @@ function addTask() {
 
 <template>
   <h1>Список задач</h1>
+
   <p>Выполненные задачи: {{ completedTasks }}</p>
   <p>Невыполненные задачи: {{ unfinishedTasks }}</p>
 
+  <div>
+    <input type="radio" id="all" value="all" v-model="filter" />
+    <label for="all">Все</label>
+
+    <input type="radio" id="completed" value="completed" v-model="filter" />
+    <label for="completed">Завершенные</label>
+
+    <input type="radio" id="active" value="active" v-model="filter" />
+    <label for="active">Активные</label>
+  </div>
+
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
+    <li v-for="todo in filteredTodos" :key="todo.id">
       <input :id="todo.id" type="checkbox" v-model="todo.completed" />
       <label :for="todo.id">{{ todo.title }}</label>
     </li>
