012e8183f3697072688e84ca2508a5330f813b8e
Artyom Khaov <a.khaov@gmail.com>
2024-11-18 12:58:11 +0700

    feat: Creating ProjectsList component

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,12 +1,5 @@
 <template>
   <header class="header">
-    <img class="logo" src="@/assets/logo.svg" width="125" height="125" alt="Vue logo" />
+    Хедер
   </header>
 </template>
-
-<style scoped>
-.logo {
-  display: block;
-  margin: 0 auto;
-}
-</style>
---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface ProjectCard {
+  id: string
+  title: string
+}
+
+const props = defineProps<{
+  project: ProjectCard
+}>()
+</script>
+
+<template>
+  {{ project.title }}
+</template>
