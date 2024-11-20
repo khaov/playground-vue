3436286e5412de832ea9b98eb7db09a556291ea0
Artyon Khaov <a.khaov@gmail.com>
2024-11-20 15:08:44 +0700

    feat: creating UsersList and UsersCard components with API fetch

--- src/content/projects/1/SolutionDemo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        Компонент решения {{ $route.params.id }}
-    </div>
-</template>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export interface Navigation {
-  id: string
-  title: string
+export interface User {
+  id: number
+  name: string
 }
